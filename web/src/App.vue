6d2e14ddd8f8e78e19e3bfcb63433a56bf4d5a00
ZYT <13681756546@139.com>
2023-06-01 19:06:53 +0800

    from AcGit

---
+++ web/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <NavBar />
+  <router-view></router-view>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
+export default {
+  components: {
+    NavBar
+  },
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+body {
+  background-image: url("@/assets/images/FluentSpiral3.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
